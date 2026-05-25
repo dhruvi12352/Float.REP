d2765a0a1a729cebfbc468dd044c0cfe70321528
dhruvi12352 <dhruvigondaliya213@gmail.com>
2026-05-25 17:57:58 +0530

    Add files via upload

---
+++ Float.py
@@ -0,0 +1,140 @@
+
+x = 3.14
+y = -0.5
+
+print(x)
+print(y)
+
+
+
+
+
+x = 1.5e3
+
+print(x)
+
+
+
+
+print(float("3.14"))
+
+
+
+print(float(5))
+
+
+
+
+print(0.1 + 0.2)
+
+
+
+
+x = 5.0
+
+print(x.is_integer())
+
+
+
+y = 5.5
+
+print(y.is_integer())
+
+
+
+
+x = 0.5
+
+print(x.as_integer_ratio())
+
+
+
+
+x = 3.5
+
+print(x.hex())
+
+
+
+
+x = float.fromhex('0x1.c000000000000p+1')
+
+print(x)
+
+
+
+
+x = -3.14
+
+print(abs(x))
+
+
+x = 3.14159
+
+print(round(x, 2))
+
+
+
+
+import math
+
+x = 3.2
+
+print(math.ceil(x))
+
+
+
+
+x = 3.9
+
+print(math.floor(x))
+
+
+
+
+x = 3.9
+
+print(math.trunc(x))
+
+
+
+
+x = float('inf')
+
+print(x)
+
+
+x = float('-inf')
+
+print(x)
+
+
+x = float('nan')
+
+print(x)
+
+
+a = 5.5
+b = 2.0
+
+print(a + b)   
+print(a - b)   
+print(a * b)   
+print(a / b)   
+print(a // b) 
+print(a % b)   
+print(a ** b)  
+
+
+a = 5.5
+b = 2.0
+
+print(a > b)
+print(a < b)
+print(a == b)
+
+
+x = 3.14
+
+print(type(x))
+
